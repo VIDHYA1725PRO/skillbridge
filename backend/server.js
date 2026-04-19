9fe075cee089a8ab5b313085c3a512a18068468e
VIDHYA <vidhyasendurpandi@gmail.com>
2026-04-19 07:37:08 +0530

    Fix backend startup by restoring path import

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 dotenv.config();
 
